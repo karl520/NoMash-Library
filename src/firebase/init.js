ab7b90cb51b3fa1ee450336695a9fe5b6ee1c86d
karlwang <zwan0482@student.monash.edu>
2025-09-19 21:53:46 +1000

    keep fix

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -3,7 +3,8 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-import { getFirestore } from "firebase/firestore"
+import { getFirestore } from "firebase/firestore";
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCNxSu258SQmF8fCyXc9zzIzvqIiQKnYx0",
